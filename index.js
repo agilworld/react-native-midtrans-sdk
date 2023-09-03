a6811c2980ab68ab69319ab93cec96335e8cc89c
Dian Afrial <agil.rahadi@gmail.com>
2023-09-03 20:48:39 +0700

    chore: upgrade sdk version

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import { NativeModules } from 'react-native';
 const { MidtransSdk } = NativeModules;
 
 export default {
-    checkOut: function (optionConect: ?object,
-                        transRequest: ?object,
-                        itemDetails: ?object,
-                        creditCardOptions: ?object,
-                        mapUserDetail: ?object,
-                        optionColorTheme: ?object,
-                        resultCheckOut) {
+    checkOut: function (
+        optionConect: ?object,
+        transRequest: ?object,
+        itemDetails: ?object,
+        creditCardOptions: ?object,
+        mapUserDetail: ?object,
+        optionColorTheme: ?object,
+        resultCheckOut) 
+    {
         MidtransSdk.checkOut(
             optionConect,
             transRequest,
